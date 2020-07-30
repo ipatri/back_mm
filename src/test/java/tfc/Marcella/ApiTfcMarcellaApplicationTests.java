493e0c80a7738677bf8f473cefa487cbbccaaa77
Isaque Patricio <isaque.tiago@yandeh.com.br>
2020-07-30 17:46:10 -0300

    first commit

---
+++ src/test/java/tfc/Marcella/ApiTfcMarcellaApplicationTests.java
@@ -0,0 +1,13 @@
+package tfc.Marcella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTfcMarcellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
